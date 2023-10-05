dc50422d84621f4497adf49854b4d71733b17a23
Bubbles <nicolas.champagne03@gmail.com>
2023-10-05 14:35:37 +0200

    kami footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 //import "./App.css";
 import Nav from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Nav name="Home" account={false}></Nav>
+      <div style={{height:500}}></div>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import "./css/footer.css";
+
+interface FooterProps{
+
+}
+
+const Footer = (props:FooterProps) => {
+    return(
+        <section>
+            <div>
+                <h1>Contact us</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Footer;

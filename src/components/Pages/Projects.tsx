2a0ae63a105d8b5ff4026c32577fd271dea07ac1
Eduard <sparqeey@gmail.com>
2023-10-16 00:51:26 +0200

    updated the projects page

--- src/components/Pages/Projects.tsx
+++ src/components/Pages/Projects.tsx
@@ -5,37 +5,46 @@ function Projects() {
   const projects = [
     {
       name: 'Project 1',
-      image: 'project1.jpg',
+      image: 'https://picsum.photos/1200/600?random=1',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       website: 'https://www.project1.com'
     },
     {
       name: 'Project 2',
-      image: 'project2.jpg',
+      image: 'https://picsum.photos/1200/600?random=2',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       website: 'https://www.project2.com'
     },
     {
       name: 'Project 3',
-      image: 'project3.jpg',
+      image: 'https://picsum.photos/1200/600?random=3',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       website: 'https://www.project3.com'
+    },
+    {
+      name: 'Project 3',
+      image: 'https://picsum.photos/1200/600?random=4',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      website: 'https://www.project4.com'
     }
   ];
 
   return (
-    <div>
-    
-      <div className="project-list">
-        {projects.map((project, index) => (
-          <div className="project-card" key={index}>
-            <h2>{project.name}</h2>
+  <div>
+    <div className="project-list">
+      {projects.map((project, index) => (
+        <a href={project.website} target="_blank" rel="noopener noreferrer" key={index} className="project-link">
+          <div className="project-card">
             <img src={project.image} alt={project.name} />
-            <a href={project.website} target="_blank" rel="noopener noreferrer">
-              <button className="visit-button">Visit Page</button>
-            </a>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
           </div>
-        ))}
-      </div>
+        </a>
+      ))}
     </div>
-  );
+  </div>
+);
+
 }
 
 export default Projects;
